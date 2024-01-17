1d8535e88f4e5aa422ba99de73f9551aa411141d
shahana <rafiyashahana@gmail.com>
2024-01-17 14:42:59 +0300

    added the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,15 @@
 import styles from "./Footer.module.css";
+import { RiCodeSSlashLine, RiReactjsLine, RiHeartFill } from "@remixicon/react";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <p>with by Rafiya Shahana using </p>
+      <p>
+        {" "}
+        <RiCodeSSlashLine size="0.7rem" color="#000" /> with{" "}
+        <RiHeartFill size="0.7rem" color="#000" />
+        by Rafiya Shahana using
+        <RiReactjsLine size="0.7rem" color="#61dbfb" />
+      </p>
       <p>
         Project code is open source. Feel free to fork and make your own
         version.
