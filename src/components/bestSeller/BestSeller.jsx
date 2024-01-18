920f2ad6e02942346ed3258c20925fa9ba3ad2b2
shahana <rafiyashahana@gmail.com>
2024-01-18 09:10:05 +0300

    made changes to bestseller Card component

--- src/components/bestSeller/BestSeller.jsx
+++ src/components/bestSeller/BestSeller.jsx
@@ -3,6 +3,7 @@ import Card from "../card/Card";
 import best1 from "../../assets/images/best1.jpeg";
 import best2 from "../../assets/images/best2.jpeg";
 import best4 from "../../assets/images/best4.jpeg";
+import { RiArrowDownDoubleLine } from "@remixicon/react";
 import styles from "./BestSeller.module.css";
 const BestSeller = () => {
   return (
@@ -12,15 +13,23 @@ const BestSeller = () => {
       <div className={styles.card_container}>
         <Card className={styles.card}>
           <img src={best2} />
-          <Button className={styles.btn}>Add to cart</Button>
+          <h4>Cheesy Pepperoni Pizza</h4>
+          <p>Tomato sauce, mozzarella, oregano, pepperoni, cheese, olives</p>
+          <Button className={styles.btn}>Add to cart (from $12)</Button>
         </Card>
+
         <Card className={styles.card}>
           <img src={best1} />
-          <Button className={styles.btn}>Add to cart</Button>
+          <h4>Vegetarian Pizza</h4>
+          <p> Cheese Mix, Tomato, Olive, Onion, Green Pepper, Mushroom</p>
+          <Button className={styles.btn}>Add to cart $13</Button>
         </Card>
+
         <Card className={styles.card}>
           <img src={best4} />
-          <Button className={styles.btn}>Add to cart</Button>
+          <h4>Spicy BBQ Pizza</h4>
+          <p>Ranch Sauce, Cheese Mix, Grilled Chicken, Onion, BBQ Swirl</p>
+          <Button className={styles.btn}>Add to cart (from $14)</Button>
         </Card>
       </div>
     </div>
