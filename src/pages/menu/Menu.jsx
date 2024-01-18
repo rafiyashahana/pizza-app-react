192368e7ed9f8c6d2aef7537709d26161f95a004
shahana <rafiyashahana@gmail.com>
2024-01-18 11:52:50 +0300

    added menu component

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,10 +1,27 @@
-import Modal from "../../components/modal/Modal";
+import Card from "../../components/card/Card";
+import Button from "../../components/button/Button";
 import styles from "./Menu.module.css";
+import best2 from "../../assets/images/best2.jpeg";
 
 const Menu = () => {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className={styles.container}>
+      <div className={styles.pizza_container}>
+        <Card className={styles.card}>
+          <img src={best2} />
+          <h4>Cheesy Pepperoni Pizza</h4>
+          <p>Tomato sauce, mozzarella, oregano, pepperoni, cheese, olives</p>
+          <Button className={styles.btn}>Add to cart (from $12)</Button>
+        </Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
     </div>
   );
 };
